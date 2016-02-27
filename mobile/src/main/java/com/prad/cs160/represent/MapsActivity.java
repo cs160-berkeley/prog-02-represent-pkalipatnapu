7c414fbcedc766778d6a181513b6b3f0dc82aa19
Pradeep Kalipatnapu <prad.me@gmail.com>
2016-02-27 00:01:34 -0800

    Zip code from map, and start new blank activity

---
+++ mobile/src/main/java/com/prad/cs160/represent/MapsActivity.java
@@ -0,0 +1,90 @@
+package com.prad.cs160.represent;
+
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toolbar;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnCameraChangeListener;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private EditText zipcode;
+    private Geocoder gcoder;
+
+    public final static String ZIP_CODE = "com.prad.cs160.represent.ZIP_CODE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mapFragment.getActivity().setActionBar(toolbar);
+        mapFragment.getActivity().getActionBar().setDisplayShowTitleEnabled(true);
+
+        gcoder = new Geocoder(this, Locale.getDefault());
+
+        zipcode = (EditText) findViewById(R.id.zip_code);
+    }
+
+    public void lookupReps(View view) {
+        /* TODO(prad): send to watch.
+        Intent sendIntent = new Intent(getBaseContext(), PhoneToWatchService.class);
+        sendIntent.putExtra("CAT_NAME", "Fred");
+        startService(sendIntent);
+        */
+        Intent intent = new Intent(this, Representatives.class);
+        String zip = zipcode.getText().toString();
+        intent.putExtra(ZIP_CODE, zip);
+        startActivity(intent);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        LatLng berkeley = new LatLng(37.87, -122.27);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(berkeley, 14.0f));
+        setZipcode(berkeley);
+        mMap.setOnCameraChangeListener(new OnCameraChangeListener() {
+            @Override
+            public void onCameraChange(CameraPosition cameraPosition) {
+                LatLng newCenter = mMap.getCameraPosition().target;
+                setZipcode(newCenter);
+            }
+        });
+    }
+
+    private void setZipcode(LatLng location) {
+        List<Address> addresses;
+        try {
+            addresses = gcoder.getFromLocation(location.latitude, location.longitude, 1);
+            zipcode.setText(addresses.get(0).getPostalCode());
+        } catch (Exception e) {
+            // Do nothing.
+        }
+    }
+}
