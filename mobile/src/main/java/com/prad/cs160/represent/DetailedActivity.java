7f646de753fe74e39f6f9f5b665eb914451a7443
Pradeep Kalipatnapu <prad.me@gmail.com>
2016-03-01 12:37:12 -0800

    Frame 3, wrap up basic mobile app.

--- mobile/src/main/java/com/prad/cs160/represent/DetailedActivity.java
+++ mobile/src/main/java/com/prad/cs160/represent/DetailedActivity.java
@@ -1,13 +1,38 @@
 package com.prad.cs160.represent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class DetailedActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detailed);
+
+        Bundle bundle = getIntent().getExtras();
+        String rep_name = bundle.getString(Representatives.REP_NAME);
+        boolean democrat = bundle.getBoolean(Representatives.PARTY);
+
+        LinearLayout banner = (LinearLayout) findViewById(R.id.banner);
+        ImageView dp = (ImageView) findViewById(R.id.displaypic);
+        TextView name = (TextView) findViewById(R.id.name);
+        TextView email = (TextView) findViewById(R.id.email);
+
+        // Set Background color
+        if (democrat) banner.setBackgroundColor(Color.parseColor("#2F80ED"));
+        else banner.setBackgroundColor(Color.parseColor("#ED2F2F"));
+
+        // Set Image
+        dp.setImageResource(this.getResources().getIdentifier(rep_name.replace(' ', '_').toLowerCase(), "drawable", getPackageName()));
+
+        // Set rep name
+        name.setText(rep_name);
+
+        // Set body details
+        email.setText(rep_name.replace(' ','_').toLowerCase()+ "@gmail.com" + "\nwww." +rep_name.replaceAll("\\s+", "").toLowerCase()+".com");
     }
 }
--- mobile/src/main/java/com/prad/cs160/represent/Representatives.java
+++ mobile/src/main/java/com/prad/cs160/represent/Representatives.java
@@ -19,8 +19,10 @@ import java.util.Map;
 public class Representatives extends AppCompatActivity {
     LinearLayout reps, senators, page;
     Map<Integer, String> rep_name;
+    Map<String, Boolean> party_dem;
 
     public final static String REP_NAME = "com.prad.cs160.represent.REP_NAME";
+    public final static String PARTY = "com.prad.cs160.represent.PARTY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,31 +34,39 @@ public class Representatives extends AppCompatActivity {
         page = (LinearLayout) findViewById(R.id.page);
 
         rep_name = new HashMap<Integer, String>();
+        party_dem = new HashMap<String, Boolean>();
 
         Bundle bundle = getIntent().getExtras();
         int zip = bundle.getInt(MapsActivity.ZIP_CODE);
+        populateData();
         populateLists(zip);
+    }
+
+    private void populateData() {
+        party_dem.put("Ted Cruz", false);
+        party_dem.put("Jeb Bush", false);
+        party_dem.put("Bernie Sanders", true);
     }
 
     private void populateLists(int zip) {
         // For even zip codes, the representative is Jeb Bush.
         // Otherwise, the representatives are Hillary Clinton and Marco Rubio.
         if (zip % 2 == 0) {
-            reps.addView(createRepBanner(false, "Jeb Bush"));
+            reps.addView(createRepBanner("Jeb Bush"));
         } else {
             // TODO(prad): Fix this idiocy.
-            senators.addView(createRepBanner(false, "Ted Cruz"));
-            senators.addView(createRepBanner(false, "Ted Cruz"));
+            reps.addView(createRepBanner("Ted Cruz"));
+            reps.addView(createRepBanner("Ted Cruz"));
         }
 
         // If the zip starts with an even number, the senators are Bernie Sanders and
         // Ted Cruz. Otherwise, they are Donald Trump and Joe Biden.
         if (zip/10000 % 2 == 0) {
-            senators.addView(createRepBanner(true, "Bernie Sanders"));
-            senators.addView(createRepBanner(false, "Ted Cruz"));
+            senators.addView(createRepBanner("Bernie Sanders"));
+            senators.addView(createRepBanner("Ted Cruz"));
         } else {
-            senators.addView(createRepBanner(false, "Ted Cruz"));
-            senators.addView(createRepBanner(false, "Ted Cruz"));
+            senators.addView(createRepBanner("Ted Cruz"));
+            senators.addView(createRepBanner("Ted Cruz"));
         }
     }
 
@@ -67,12 +77,13 @@ public class Representatives extends AppCompatActivity {
             String name = rep_name.get(selected_representative);
             Intent intent = new Intent(Representatives.this, DetailedActivity.class);
             intent.putExtra(REP_NAME, name);
+            intent.putExtra(PARTY, party_dem.get(name));
             startActivity(intent);
         }
     };
 
 
-    private LinearLayout createRepBanner(boolean democrat, String name) {
+    private LinearLayout createRepBanner(String name) {
         // Create a red banner for repiblicans, and a blue one for democrats.
         LinearLayout rep = new LinearLayout(this);
         // Create a mapping from id to name.
@@ -87,8 +98,8 @@ public class Representatives extends AppCompatActivity {
         rep.setLayoutParams(LLParams);
         rep.setGravity(Gravity.CENTER_VERTICAL);
         rep.setPadding(10,10,10,10);
-        if (democrat) rep.setBackgroundColor(Color.BLUE);
-        else rep.setBackgroundColor(Color.RED);
+        if (party_dem.get(name)) rep.setBackgroundColor(Color.parseColor("#2F80ED"));
+        else rep.setBackgroundColor(Color.parseColor("#ED2F2F"));
         rep.setOrientation(LinearLayout.HORIZONTAL);
 
         ImageView iv = new ImageView(this);
